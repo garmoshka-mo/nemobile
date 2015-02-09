f7cc7615038f200887bc0982bcf0019310ee2bcf
Степанов Давид <stepanov@pirsipy.com>
2015-02-09 17:53:19 +0300

    minor fixes

--- www/js/filters.js
+++ www/js/filters.js
@@ -19,3 +19,23 @@ app.filter('orderObjectBy', function() {
     return filtered;
   };
 });
+
+app.filter('orderByName', function() {
+  return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      if (a.displayName < b.displayName) {
+        return -1;
+      }
+      else {
+        return 1;
+      }
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+});
+
