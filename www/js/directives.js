e906647c23c4e0bbc0bb0de4339633661b326280
Степанов Давид <stepanov@pirsipy.com>
2015-02-10 23:08:26 +0300

    minor fixes

--- www/js/directives.js
+++ www/js/directives.js
@@ -71,7 +71,7 @@ app.directive('stickersGalleryHeight', function() {
 
 app.directive('spinner', function() {
     return {
-        template: '<div class="spinner"><div class="dot dot1"></div><div class="dot dot2"></div></div>',
+        template: '<div class="spinner"><div class="dot bounce1"></div><div class="dot bounce2"></div><div class="dot bounce3"></div></div>',
         link: function(scope, elem, attr) {
             var dots = $(elem).find(".dot");
             if (attr.spinnerColor == "grey") {
