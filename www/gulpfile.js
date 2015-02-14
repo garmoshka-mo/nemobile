6bd7885b9369462888d531ff13e7058103250963
Степанов Давид <stepanov@pirsipy.com>
2015-02-14 18:30:51 +0300

    installed gulp

---
+++ www/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+var uglify = require('gulp-uglifyjs');
+ 
+gulp.task('uglify', function() {
+  gulp.src('public/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('default', function(){
+  // Default task code
+});
