1ffd804bd573c048ffc4062fa497343603063ff7
Степанов Давид <stepanov@pirsipy.com>
2015-02-20 12:20:36 +0300

    minor fixes

--- www/js/ang-controllers/exitController.js
+++ www/js/ang-controllers/exitController.js
@@ -2,6 +2,6 @@ angular.module("angControllers").controller("exitController", ['$scope', '$state
     $scope.exit = function() {
         user.logout();
         $state.go('start');
-    }
-}])
+    };
+}]);
     
