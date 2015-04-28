6353e0fab43d37d1a03eedd39c114c24c6fd0904
Степанов Давид <stepanov@pirsipy.com>
2015-04-28 18:19:54 +0300

    minor fixes

---
+++ www/js/ang-controllers/vkLoginController.js
@@ -0,0 +1,15 @@
+angular.module("angControllers").controller("vkLoginController", [
+    'user', '$scope', '$http', '$timeout', 'vk', '$stateParams',
+    function(user, $scope, $http, $timeout, vk, $stateParams) {
+        var accessToken = $stateParams.access_token;
+        var userId = $stateParams.user_id;
+        if (accessToken && userId) {
+            vk.auth(accessToken, userId)
+            .then(function() {
+                vk.getUser()
+                .then(function(res) {
+                    console.log(res);
+                });
+            });
+        }    
+}]);
