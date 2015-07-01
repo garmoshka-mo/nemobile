847bcf2e9098a9aff618abf444d096172065fc45
Степанов Давид <stepanov@pirsipy.com>
2015-07-01 12:43:54 +0300

    minor fixes

--- www/js/ang-controllers/startController.js
+++ www/js/ang-controllers/startController.js
@@ -1,20 +1,28 @@
 angular.module("angControllers").controller("startController", ['user', '$state', function(user, $state) {
+    
+    function hideSplashScreen() {
+        if (window.device) {
+            navigator.splashscreen.hide();
+        }
+    }
+
     if (user.isLogged()) {
         user.parseFromStorage()
         .then(
             function() {
                 if (Object.keys(user.chats).length === 0) {
-                    $state.go('friends');
+                    $state.go('friends')
+                    .then(
+                        hideSplashScreen
+                    );
                 }
                 else {
-                    $state.go('chats');
+                    $state.go('chats')
+                    .then(
+                        hideSplashScreen
+                    );
                 }
                 
-                if (window.device) {
-                    setTimeout(function() {
-                        navigator.splashscreen.hide();
-                    }, 100);
-                }
                 console.log("user data is taken from storage");
             }
         );
