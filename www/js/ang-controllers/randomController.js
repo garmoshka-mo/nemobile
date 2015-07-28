1f6294e2b588eea0ce62a34f757bb9ad52233685
Степанов Давид <stepanov@pirsipy.com>
2015-07-28 18:52:37 +0300

    minor fixes

--- www/js/ang-controllers/randomController.js
+++ www/js/ang-controllers/randomController.js
@@ -1,6 +1,16 @@
 angular.module("angControllers").controller("randomController", [
          'user', '$scope',
     function(user, $scope) {
-       $scope.opened = true;
+        $scope.opened = true;
+
+        $scope.iam = {
+            sex: 'man',
+            age: '1'
+        }
+
+        $scope.another = {
+            sex: 'woman',
+            age: '2'
+        }
     }
 ]);
