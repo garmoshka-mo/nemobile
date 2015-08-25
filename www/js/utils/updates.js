2c0b757cbf8130ad5510b90c4cbc1413d83b35cf
dan <dan@coav.ru>
2015-08-25 17:49:17 +0300

    skip updates for APP

--- www/js/utils/updates.js
+++ www/js/utils/updates.js
@@ -7,6 +7,7 @@ function Service($resource) {
     var VersionResource = $resource('/version');
 
     this.check = function() {
+        if (RAN_AS_APP) return;
         // Automatic reload if server updated:
         var v = VersionResource.get(function() {
             if (v.version != version) location.reload();
