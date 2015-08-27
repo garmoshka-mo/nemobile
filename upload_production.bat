7e9cc8a02552fce722d9fa72bfadf2dc972174b5
Степанов Давид <stepanov@pirsipy.com>
2015-08-27 12:01:35 +0300

    minor fixes

---
+++ upload_production.bat
@@ -0,0 +1,7 @@
+cd build
+git add remote origin git@github.com:garmoshka-mo/nepotom.git
+git add remote prod https://git.heroku.com/nemobile-prod.git
+git add .
+git commit -m "new version"
+git push origin master:production
+git push prod master
