5f0f6dd5afd0935f3daea26a847b3c2966ac43e9
dan <dan@coav.ru>
2015-09-02 23:38:11 +0300

    fixes

--- www/app/chats/SpamFilter.js
+++ www/app/chats/SpamFilter.js
@@ -17,7 +17,7 @@ function($resource, $q) {
             }
 
             var data = {
-                client_id: user_uuid, // todo: replace to user.uuid after refactoring of user (right now creates circular dep)
+                client_id: user_uuid, // todo: replace to user.safe_id after refactoring of user (right now creates circular dep)
                 talk_id: session.uuid, // identification of session, can be used by async callback
                 // to inform about malicious session
                 timestamp_ms: Date.now(),
