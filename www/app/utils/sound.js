e990aa250ff4f4b4849af26977dcbfeffa16b26d
Степанов Давид <davstepanov@gmail.com>
2015-09-14 15:20:37 +0300

    minor fixes

--- www/app/utils/sound.js
+++ www/app/utils/sound.js
@@ -1,9 +1,9 @@
 (function () {
     services
         .value('sounds', {
-            // incomeMessage: new Audio('/android_assets/www/assets/sounds/alert.mp3'),
-            // newConversation: new Audio('/android_assets/www/assets/sounds/new_conversation.mp3'),
-            incomeMessage_android: new Media('/android_assets/www/assets/sounds/alert.mp3')
+            incomeMessage: new Audio('file:///android_asset/www/assets/sounds/alert.mp3'),
+            newConversation: new Audio('file:///android_asset/www/assets/sounds/new_conversation.mp3'),
+            // incomeMessage_android: new Media('/android_assets/www/assets/sounds/alert.mp3')
         })
         .service('sound',
         ['sounds', 'deviceInfo',
