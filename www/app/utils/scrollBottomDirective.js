eedc590325f4cbe92b01ce489fab0c246983ae07
Степанов Давид <davstepanov@gmail.com>
2015-09-23 12:04:42 +0300

    minor fixes

--- www/app/utils/scrollBottomDirective.js
+++ www/app/utils/scrollBottomDirective.js
@@ -1,4 +1,5 @@
 angular.module("angApp").directive('scrollBottom', ['$timeout', function ($timeout) {
+    //scrolls to top of element
     return {
         scope: {
             scrollBottom: '='
@@ -8,9 +9,10 @@ angular.module("angApp").directive('scrollBottom', ['$timeout', function ($timeo
             var $container = $('section.main-section');
             scope.$watch('scrollBottom', function(newValue, oldValue) {
                 $timeout(function() {
+                    var TOP_BAR_HEIGHT = 45;
                     if (newValue) 
                         $container.animate({
-                            scrollTop: $container.scrollTop() + $(element).offset().top - 45
+                            scrollTop: $container.scrollTop() + $(element).offset().top - TOP_BAR_HEIGHT
                         }, 300);
                 }, 0);
             });
