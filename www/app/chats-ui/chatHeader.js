431f1cf93773934a3b65eb0f880ccb317a89d817
Basil <Syperia@gmail.com>
2015-10-07 12:01:45 +0300

    chatHeader.me updates normally

--- www/app/chats-ui/chatHeader.js
+++ www/app/chats-ui/chatHeader.js
@@ -62,7 +62,7 @@ function($rootScope, user) {
 
     user.passivePromise.then(function(){
         if (user.myScores) {
-            self.me.ava_url = user.avatar.urlMini;
+            self.me.ava = user.avatar;
             self.me.hidden = false;
             user.myScores.updateUI = updateUI.bind(null, self.me);
             user.myScores.ask();
