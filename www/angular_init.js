723b72ddd704a85692a3ee6284e07c67808185b8
Степанов Давид <davstepanov@gmail.com>
2015-10-08 14:07:04 +0300

    with dragging

--- www/angular_init.js
+++ www/angular_init.js
@@ -1,7 +1,7 @@
 angular.module("angApp", ["ui.router", "angControllers", "angServices", "angFactories",
     "LocalForageModule", "ngTouch", "ngSanitize", "angularFileUpload", /*"ngAnimate",*/
     "internationalPhoneNumber", "infinite-scroll", "ngSocial", "ngMaterial", 'ngResource',
-    '720kb.tooltips', 'angular-inview']);
+    '720kb.tooltips', 'angular-inview', 'hmTouchEvents']);
 angular.module("angControllers", []);
 angular.module("angServices", []);
 angular.module("angFactories", []);
--- www/app/core-ui/menuDirective.js
+++ www/app/core-ui/menuDirective.js
@@ -7,38 +7,132 @@
         return {
             templateUrl: "app/core-ui/menu.html?"+version,
             link: function($scope, element) {
+                var ALLOWED_ANGLE = 45;
+                var allowOpenning = false;
+                var $menu = $(element);
                 var $menuContent = $('.menu-content'); 
                 var $whenMenuOpenDiv = $('<div class="when-menu-open"></div>');
                 $menuContent.append($whenMenuOpenDiv);
 
+                function onOpenClose() {
+                    addAnimation();
+                    removeTransform();
+                    menu.openning = false;
+                    allowOpenning = false;
+                }
+
                 menu.open = function() {
-                    $(element).addClass('menu-open');
+                    onOpenClose();
+                    $menu.addClass('menu-open');
                     $whenMenuOpenDiv.fadeIn('slow');
                 };
 
                 menu.close = function() {
-                    $(element).removeClass('menu-open');
+                    onOpenClose();
+                    $menu.removeClass('menu-open');
                     $whenMenuOpenDiv.fadeOut('slow');
                 };
 
                 menu.toggle = function() {
-                    $(element).hasClass('menu-open') ? menu.close() : menu.open();
+                    $menu.hasClass('menu-open') ? menu.close() : menu.open();
                 };
 
                 menu.setWidth = function(width) {
                     menu.width = width; 
-                    $(element).width(width + 'px');
+                    $menu.width(width + 'px');
                 };
+
+                var menuHammer = new Hammer($menuContent.get(0), {
+                    inputClass: Hammer.TouchMouseInput
+                });
+
+                var previousPanX = 0;
+                function calculateNextX(event) {
+
+                    if (previousPanX === 0) {
+                        removeAnimation();
+                    }
+                    // console.log(event.deltaX);
+                    var previousX = -1 * menu.width;
+                    if ($menu.css('transform')) {
+                        previousX = getCurrentX();
+                    }
+                    var distance = event.deltaX - previousPanX;
+                    previousPanX = event.deltaX;
+                    return  previousX + distance;
+                }
+                
+                menuHammer.on('pan', function(event) {
+                    if (!allowOpenning) {
+                        return;
+                    }
+                    console.log('pan');
+                    var nextX = calculateNextX(event);
+                    menu.openning = true; 
+                    // console.log(nextX);
+                    if (nextX > 0) {
+                        menu.open();
+                        return;
+                    }
+                    $menu.css('transform', 'translate3d(' + nextX + 'px, 0,0)');
+                    // console.log(event);
+                });
+
+                menuHammer.on('panend', function() {
+                    previousPanX = 0;
+                    allowOpenning = false;
+                    console.log('touch end!');
+                    if (menu.openning) {
+                        finishAction();
+                    }
+                });
+
+                menuHammer.on('panstart', function(event) {
+                    if (event.angle < 45 && event.angle > -45) {
+                        allowOpenning = true;
+                    } 
+                    else {
+                        allowOpenning = false;
+                    }
+                });
+
+                function removeTransform() {
+                    $menu.css('transform', '');
+                }
+
+                function addAnimation() {
+                    $menu.addClass('menu-animated');
+                }
+
+                function removeAnimation() {
+                    $menu.removeClass('menu-animated');
+                }
+
+                function finishAction() {
+                    var currentX = getCurrentX();
+                    if (currentX > (-1 * menu.width / 2)) {
+                        menu.open();
+                    }
+                    else {
+                        menu.close();
+                    }
+                }
+
+                function getCurrentX() {
+                    var transformString = $menu.css('transform');
+                    return +transformString.match(/matrix\(\d+, \d+, \d+, \d+, ([-]*\d+\.*\d*), \d\)/)[1];
+                }
 
                 menu.setWidth(250);
                 $whenMenuOpenDiv.click(menu.close);
-
-
-                        
             }
             // controller: ['$scope', '$timeout', 'deviceInfo', 'router', controller]
         };
     }]);
+
+
+
+   
     
 
 })();
