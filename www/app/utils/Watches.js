9d50756eeef5532b652cafc4a97cf605d3cb6aa7
dan <dan@coav.ru>
2015-10-15 20:33:09 +0300

    fix watches

--- www/app/utils/Watches.js
+++ www/app/utils/Watches.js
@@ -15,22 +15,32 @@ function Watches(done) {
         done();
     });
 
+    function isWorkingTime(h){
+        return h >= self.openAt &&  h < self.closeAt;
+    }
+
     self.left = function(dots) {
         var serverTime = new Date(Date.now() - timeDifferenceWithServer),
-            h = self.openAt - serverTime.getHours(),
-            m = 60 - serverTime.getMinutes();
+            h = self.openAt - serverTime.getHours() - 1,
+            m = 59 - serverTime.getMinutes();
 
-        if (isWorkingTime(h)) {
-            location.reload();
+        if (h < 0) {
+            return '0:00:00';
         }
 
         if (m < 10) m = '0'+m;
 
-        return (h - 1) + (dots ? ":" : " ") + m;
+        var time = h + (dots ? ":" : " ") + m;
+
+        if (h + m == 0) {
+            var s = 59 - serverTime.getSeconds();
+            if (s < 10) s = '0'+s;
+            time += (dots ? ":" : " ") + s;
+        }
+
+        return time;
     };
 
-    function isWorkingTime(h){
-        return h > self.openAt &&  h < self.closeAt;
-    }
+
 
 }
