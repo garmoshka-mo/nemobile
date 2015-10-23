ba4a94dbbef485aeca6f8e6ee72a8d6e19994e57
Basil <Syperia@gmail.com>
2015-10-23 13:06:23 +0300

    input to div

--- www/angular_init.js
+++ www/angular_init.js
@@ -1,7 +1,7 @@
 angular.module("angApp", ["ui.router", "angControllers", "angServices", "angFactories",
     "LocalForageModule", "ngTouch", "ngSanitize", "ngFileUpload", /*"ngAnimate",*/
     "internationalPhoneNumber", "infinite-scroll", "ngSocial", "ngMaterial", 'ngResource',
-    '720kb.tooltips', 'angular-inview', 'hmTouchEvents', 'ngCookies', 'pascalprecht.translate',
+    '720kb.tooltips', 'angular-inview', 'hmTouchEvents', 'ngCookies', 'pascalprecht.translate','emoji',
     'ngTagsInput']);
 angular.module("angControllers", []);
 angular.module("angServices", []);
